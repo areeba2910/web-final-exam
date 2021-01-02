2300bc7b056b88f93dd3c84fe437171729295be0
areeba2910 <areeba2910@users.noreply.github.com>
2021-01-03 01:03:13 +0500

    .

--- composer.js
+++ composer.js
@@ -37,10 +37,7 @@ $(document).ready(function(){
         var ratingValue = parseInt($('#stars li.selected').last().data('value'), 10);
         var msg = "";
         if (ratingValue > 1) {
-            msg = "Thanks! You rated this " + ratingValue + " stars.";
-        }
-        else {
-            msg = "We will improve ourselves. You rated this " + ratingValue + " stars.";
+            msg = "thanks for your feedback " + ratingValue + " stars.";
         }
         responseMessage(msg);
         
